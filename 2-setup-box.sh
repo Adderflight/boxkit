dfd91d716f7210bbacc52fecbf8326b554f5233d
adderflight <adderflight1@protonmail.com>
2025-08-08 10:48:41 -0400

    remove protonmail steps

--- 2-setup-box.sh
+++ 2-setup-box.sh
@@ -3,11 +3,6 @@
 # export chromium browser
 distrobox-export -a chromium-browser
 
-# install proton mail
-sudo dnf install -y /home/adderflight/syncthing/programs-appimages/packages/ProtonMail-desktop-beta.rpm
-
-distrobox-export -a proton-mail
-
 # export binaries
 distrobox-export -b /usr/bin/nmap -ep /home/adderflight/.local/bin
 distrobox-export -b /usr/bin/httrack -ep /home/adderflight/.local/bin
